71c5296088b1932e5e4a874b7db380d836567d1c
sheilla mungai <mungaisheilla19@gmail.com>
2025-06-02 19:59:54 +0300

    first commit

---
+++ queries.js
@@ -0,0 +1,188 @@
+db.books.insertMany([
+    {title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Fiction',
+    published_year: 1960,
+    price: 12.99,
+    in_stock: true,
+    pages: 336,
+    publisher: 'J. B. Lippincott & Co.'},
+    {
+    title: '1984',
+    author: 'George Orwell',
+    genre: 'Dystopian',
+    published_year: 1949,
+    price: 10.99,
+    in_stock: true,
+    pages: 328,
+    publisher: 'Secker & Warburg'
+  },
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    genre: 'Fiction',
+    published_year: 1925,
+    price: 9.99,
+    in_stock: true,
+    pages: 180,
+    publisher: 'Charles Scribner\'s Sons'
+  },
+  {
+    title: 'Brave New World',
+    author: 'Aldous Huxley',
+    genre: 'Dystopian',
+    published_year: 1932,
+    price: 11.50,
+    in_stock: false,
+    pages: 311,
+    publisher: 'Chatto & Windus'
+  },
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    genre: 'Fantasy',
+    published_year: 1937,
+    price: 14.99,
+    in_stock: true,
+    pages: 310,
+    publisher: 'George Allen & Unwin'
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    genre: 'Fiction',
+    published_year: 1951,
+    price: 8.99,
+    in_stock: true,
+    pages: 224,
+    publisher: 'Little, Brown and Company'
+  },
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    genre: 'Romance',
+    published_year: 1813,
+    price: 7.99,
+    in_stock: true,
+    pages: 432,
+    publisher: 'T. Egerton, Whitehall'
+  },
+  {
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    genre: 'Fantasy',
+    published_year: 1954,
+    price: 19.99,
+    in_stock: true,
+    pages: 1178,
+    publisher: 'Allen & Unwin'
+  },
+  {
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    genre: 'Political Satire',
+    published_year: 1945,
+    price: 8.50,
+    in_stock: false,
+    pages: 112,
+    publisher: 'Secker & Warburg'
+  },
+  {
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    genre: 'Fiction',
+    published_year: 1988,
+    price: 10.99,
+    in_stock: true,
+    pages: 197,
+    publisher: 'HarperOne'
+  },
+  {
+    title: 'Moby Dick',
+    author: 'Herman Melville',
+    genre: 'Adventure',
+    published_year: 1851,
+    price: 12.50,
+    in_stock: false,
+    pages: 635,
+    publisher: 'Harper & Brothers'
+  },
+  {
+    title: 'Wuthering Heights',
+    author: 'Emily Brontë',
+    genre: 'Gothic Fiction',
+    published_year: 1847,
+    price: 9.99,
+    in_stock: true,
+    pages: 342,
+    publisher: 'Thomas Cautley Newby'
+  }
+]
+)
+
+//Basic CRUD operations
+db.books.find({ genre: 'Fiction'})
+
+db.books.find({ published_year:{$gt: 1951} })
+
+db.books.find({ author: 'Paulo Coelho'});
+
+db.books.updateOne({title:"The Lord of the Rings"}, {$set:{price:20.99}});
+
+db.books.deleteOne( { title:"The Great Gatsby"});
+
+//Advanced Queries
+db.books.find({
+  $and:[
+    {in_stock:true},
+    {published_year:{$gt:2010} }
+  ]
+});
+
+db.books.find( {}, {
+  title:1, author:1, price:1, _id:0
+});
+
+db.books.find().sort({ price: 1});
+
+db.books.find().sort({ price: -1});
+
+db.books.find().skip(0).limit(5);
+
+
+//Aggretation pipeline
+db.books.aggregate([
+  { $group: { _id: "$genre", averagePrice: { $avg: "$price" } } }
+]);
+
+db.books.aggregate([
+  { $group: { _id: "$author", bookCount: { $sum: 1 } } },
+  { $sort: { bookCount: -1 } },
+  { $limit: 1 }
+]);
+
+db.books.aggregate([
+  {
+    $project: {
+      decade: { $subtract: ["$published_year", { $mod: ["$published_year", 10] }] }
+    }
+  },
+  {
+    $group: {
+      _id: "$decade",
+      bookCount: { $sum: 1 }
+    }
+  },
+  { $sort: { _id: 1 } }
+]);
+
+
+//Indexing
+db.books.createIndex({ title: 1 });
+
+db.books.createIndex({ author: 1, published_year: -1 });
+
+db.books.find({ title: 'The Alchemist' }).explain("executionStats");
+
+
+
